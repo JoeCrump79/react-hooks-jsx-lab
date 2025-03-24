b0e0aaff89fc86d4b694cf2011da8020d9cb0eb6
Joseph Crump <josephcrump79@yahoo.com>
2025-03-24 14:16:20 -0500

    react-hooks-jsx-lab completed

---
+++ src/data/user.js
@@ -0,0 +1,8 @@
+// use these variables in your JSX (update them with your own name and city!)
+export const name = "YourName";
+export const city = "YourCity";
+export const image = "https://via.placeholder.com/150";
+
+
+
+
